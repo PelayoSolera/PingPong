3bc51f0945c10763b2858a23991dd1661486aaf3
sergiosanz30 <sergio.sanz@solera.com>
2023-07-04 16:59:14 +0200

    Uptade User variables

--- pingPongBack/src/main/java/com/solera/pingPongBack/model/User.java
+++ pingPongBack/src/main/java/com/solera/pingPongBack/model/User.java
@@ -14,7 +14,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    private String firstname;
     private String lastname;
     private String email;
     private String password;
