5e3546891fe61c05f251acb1e18f4ab211a3c5bc
Pelayo Perez <pelayo.perezdeayala@Solera.com>
2023-07-17 16:47:17 +0200

    add create bank functionality

---
+++ pingPongFront/src/Components/BankAccountItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BankAccountItem({key, bankAccount}) {
+  return (
+    <div>
+        <p key={key}>{bankAccount.accountName}</p>
+    </div>
+  )
+}
+
+export default BankAccountItem
